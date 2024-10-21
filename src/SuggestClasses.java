035695129d4032a1185dbf8217cd09f2f419574f
helenf1 <helenf1@gmail.com>
2024-10-21 17:35:15 -0400

    Creating methods

--- src/SuggestClasses.java
+++ src/SuggestClasses.java
@@ -5,19 +5,20 @@ public class SuggestClasses {
     // main choice thing
     public static void main(String[] args) {
         // maybe will ask this somewhere else but wanted to make this section functional vv
-        System.out.println("What is your major? Please write it in full.");
+        System.out.println("What is your major? Please write it in full using title caps. (ex. Biochemistry and Molecular Biology)");
         Scanner input = new Scanner(System.in);
         String major = input.nextLine();
 
         switch (major) {
-            case "Information Systems", "Information systems", "information systems":
-                informationSystems();
-                break;
-            case "Computer Science", "computer science", "Computer science":
-                computerScience();
-                break;
-                // etc. for all the majors
+            case "Information Systems", "Information systems", "information systems", "IS", "is" -> informationSystems();
+            case "Computer Science", "computer science", "Computer science" -> computerScience();
+            case "Biochemistry", "biochemistry", "Biochemistry and Molecular Biology", "biochemistry and molecular biology" -> bioChemistry();
+            case "Bioinformatics", "Bioinformatics and Computational Biology" -> bioInformatics();
+            case "Biology", "Biological Sciences", "Biological sciences" -> biologicalSciences();
+            default -> System.out.println("Invalid major.");
+            // etc. for all the majors
         }
+        generalEducation();
     }
 
 
@@ -29,11 +30,33 @@ public class SuggestClasses {
         System.out.println("IS147: Introduction to Computer Programming");
         System.out.println("MGMT210: The Practice of Management");
         System.out.println("ECON102: Principles of Macroeconomics");
-        generalEducation();
     }
 
     public static void computerScience(){
+        System.out.println("As an Information Systems major, here are the major classes you can take with no prerequisites:");
+        System.out.println("CMSC201: Computer Science I");
+        System.out.println("MATH151: Calculus & Analytic Geometry");
+    }
 
+    public static void bioChemistry() {
+        System.out.println("As a Biochemistry & Molecular Biology major, here are the major classes you can take with no prerequisites:");
+        System.out.println("MATH151: Calculus and Analytic Geometry");
+        System.out.println("CHEM101: Principles of Chemistry 1");
+        System.out.println("BIO141: Foundations of Biology: Cells, Energy, & Org.");
+    }
+
+    public static void bioInformatics() {
+        System.out.println("As a Bioinformatics & Computational Biology major, here are the major classes you can take with no prerequisites:");
+        System.out.println("MATH151: Calculus and Analytic Geometry");
+        System.out.println("CHEM101: Principles of Chemistry 1");
+        System.out.println("BIO141: Foundations of Biology: Cells, Energy, & Org.");
+    }
+
+    public static void biologicalSciences() {
+        System.out.println("As a Biological Sciences major, here are the major classes you can take with no prerequisites:");
+        System.out.println("MATH151: Calculus and Analytic Geometry");
+        System.out.println("CHEM101: Principles of Chemistry 1");
+        System.out.println("BIO141: Foundations of Biology: Cells, Energy, & Org.");
     }
 
 
