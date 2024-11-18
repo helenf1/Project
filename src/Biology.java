663c6cf08767bd48edd170a686e82b16339fe2fd
kristineataviado <katavia2@umbc.edu>
2024-11-18 17:32:44 -0500

    biology four year plans and suggest majors class

--- src/Biology.java
+++ src/Biology.java
@@ -6,7 +6,7 @@ public class Biology {
     }
 
     public static void askTrack() {
-        System.out.println("Which kind of biology are you? \n1: Biological Sciences B.A \n2: Biological Sciences B.S \n3: Biology Education \n4: Biochemistry & Molecular Biology \n5: Bioinformatics & Computational Biology");
+        System.out.println("Which kind of biology are you? \n1: Biological Sciences B.A \n2: Biological Sciences B.S \n3: Biology Education");
         Scanner input = new Scanner(System.in);
         int type = input.nextInt();
         switch (type) {
@@ -24,9 +24,9 @@ public class Biology {
         Scanner input = new Scanner(System.in);
         int type = input.nextInt();
         switch (type) {
-            case 1 -> bioFourYearPlan(" ENME110\n"," CHEM351L\n"," CHEM301 CHEM311L CHEM302\n"," ENCH437L\n");
-            case 2 -> bioFourYearPlan(" BIOL141\n"," BIOL303 CHEM352\n", " CHEM437 CHEM303\n"," ENCH482 ENCH485L\n");
-            case 3 -> bioFourYearPlan(" ENME110\n"," ENCH2101 CHEM352\n"," ENCH410 CHEM303\n"," ENCH437L ENCH414 ENCH412\n");
+            case 1 -> bioFourYearPlan(" BIO430\n"," CHEM437 \n"," PHYS111\n"," PHYS121\n");
+            case 2 -> bioFourYearPlan(" STAT 121\n"," CHEM 123 \n", " CHEM124\n"," BIOL273L\n");
+            case 3 -> bioFourYearPlan(" BIO300L\n"," PHYS122l\n"," CHEM352 CHEM352L\n"," BIOL430 CHEM 437\n");
             default -> System.out.println("Invalid option.");
         }
     }
@@ -85,27 +85,26 @@ public class Biology {
 
     public static void bioEdu() {
         System.out.println("Here is your 4 year plan!");
-        System.out.println(" Year 1: CHEM101 MATH151 ENES101 CHEM102 CHEM102L PHYS121 MATH152 ENME110");
-        System.out.println(" Year 2: ENME220 STAT355 MATH251 PHYS122 ENME221 MATH225 ENME204 ENME217");
-        System.out.println(" Year 3: CMPE306 ENME320 ENME303 ENME301 ENME304 ENME321 ENME360 ENME332L");
-        System.out.println(" Year 4: ENME403 ENME432L ENME482L ENME444");
+        System.out.println(" Year 1: BIO141 MATH155 PSYC100 BIOL142 CHEM101 GES110");
+        System.out.println(" Year 2: BIOL302 CHEM102 PSYC210 EDUC310 BIOL303 BIOL397 CHEM102L PHYS111 EDUC311");
+        System.out.println(" Year 3: BIOL300L CHEM351 PHYS112 STAT350 EDUC412 EDUC388 EDUC410");
+        System.out.println(" Year 4: BIOL302L EDUC427 EDUC411 GES311 EDUC456 EDUC457");
     }
 
-    private static final String[] bioYear1 = {"Year 1:", "CHEM101", "MATH155", "ENGL GEP",
+    private static final String[] bioYear1 = {"Year 1:", "CHEM101", "MATH151", "ENGL GEP",
             "Elective", "BIOL141", "CHEM102", "CHEM102L"};
     private static final String[] bioYear2 = {"Year 2:","BIOL142","CHEM351","BIOL302"
-            ,"GEP","GEP","Upper Level Elective"};
-    private static final String[] bioYear3 = {"Year 3:","BIOL303","BIOL300L","PHYS111","BIOL Elective",
-            "STAT350", "PHYS112"};
+            ,"MATH155"};
+    private static final String[] bioYear3 = {"Year 3:","BIOL303","BIOL300L","PHYS111","STAT350",
+            "MATH155", "PHYS112"};
     private static final String[] bioYear4 = {"Year 4:","BIOL Upper Level Lab Elective",
-            "Writing Intensive Course","BIOL Elective", "Upper Level Research"};
-    private static final String[] bioBAYear1 = {"Year 1:","CMSC201","CMSC202","PHYS121",
-            "MATH151","MATH152","CMPE212","ENES101"};
-    private static final String[] bioBAYear2 = {"Year 2:","MATH251","PHYS122","CMSC203",
-            "CMPE306L","MATH225","CMPE310","CMSC341"};
-    private static final String[] bioBAYear3 = {"Year 3:","CMPE314","CMPE311","MATH221",
+            "STAT350","BIOL Elective", "Upper Level Research"};
+    private static final String[] bioBAYear1 = {"Year 1:","CMSC202","PHYS121",
+            "MATH151","MATH152"};
+    private static final String[] bioBAYear2 = {"Year 2:","MATH251","PHYS122","MATH225"};
+    private static final String[] bioBAYear3 = {"Year 3:","BIOL141","BIOL302","MATH221",
             "CMPE320","CMPE349"};
-    private static final String[] bioBAYear4 = {"Year 4:","CMSC411","CMPE450","CMPE451"};
+    private static final String[] bioBAYear4 = {"Year 4:","BIOL303","BIOL300L","BIOL397"};
 
 
     }
--- src/SuggestMajor.java
+++ src/SuggestMajor.java
@@ -1,24 +1,121 @@
 import java.util.Scanner;
 
 public class SuggestMajor {
-    public static void suggest(){
+
+    public static void main(String[] args) {
+        suggest();
+    }
+
+    public static void suggest() {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Examples: Math, English, Science, Social Studies");
+        // Introduction message
+        System.out.println("Welcome to the Major Suggestion System!");
+        System.out.println("Let's help you explore some potential majors based on your interests.");
 
-        System.out.print("What classes are you interested in? ");
-        String interestedClasses = input.next();
+        // Get user interest
+        String interestedClasses = getUserInterest();
 
-        if(interestedClasses.equals("Math"))
-            System.out.println("You may be interested in a mathematics major, computer science major, information systems major");
-        else if(interestedClasses.equals("English"))
-            System.out.println("You may be interested in a language major");
-        else if(interestedClasses.equals("Science"))
-            System.out.println("You may be interested in a physics major, biology major or a chemistry major");
-        else if(interestedClasses.equals("Social Studies"))
-            System.out.println("You may be interested in an american history major, political science major or anthropology major");
-        else
-            System.out.println("You may want to go undecided");
+        // Offer suggestions based on the interest
+        if (interestedClasses.contains("math")) {
+            suggestMath();
+        } else if (interestedClasses.contains("english")) {
+            suggestEnglish();
+        } else if (interestedClasses.contains("science")) {
+            suggestScience();
+        } else if (interestedClasses.contains("social studies")) {
+            suggestSocialStudies();
+        } else {
+            System.out.println("\nIt seems you might have broader interests. Let's narrow it down a bit more.");
+            askFollowUpQuestions();
+        }
 
+        // Final message
+        System.out.println("\nRemember, these are just a few options! Feel free to explore many fields before deciding.");
+        System.out.println("Good luck with your major search!");
+    }
+
+    public static String getUserInterest() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("\nWhat type of subjects or classes are you most interested in? (e.g., Math, English, Science, Social Studies): ");
+        return input.nextLine().toLowerCase();
+    }
+
+    public static void suggestMath() {
+        System.out.println("\nSince you are interested in Math, here are some possible majors you might consider:");
+        System.out.println("- Mathematics");
+        System.out.println("- Computer Science");
+        System.out.println("- Information Systems");
+        System.out.println("- Engineering (e.g., Electrical, Mechanical, Civil)");
+        System.out.println("- Economics");
+        System.out.println("- Data Science");
+    }
+
+    public static void suggestEnglish() {
+        System.out.println("\nSince you are interested in English, here are some potential majors:");
+        System.out.println("- English Literature");
+        System.out.println("- Creative Writing");
+        System.out.println("- Linguistics");
+        System.out.println("- Communications");
+        System.out.println("- Journalism");
+        System.out.println("- Education");
+    }
+
+    public static void suggestScience() {
+        System.out.println("\nSince you are interested in Science, here are some possible majors for you:");
+        System.out.println("- Biology");
+        System.out.println("- Chemistry");
+        System.out.println("- Physics");
+        System.out.println("- Environmental Science");
+        System.out.println("- Astronomy");
+        System.out.println("- Geology");
+        System.out.println("- Biotechnology");
+    }
+
+    public static void suggestSocialStudies() {
+        System.out.println("\nSince you are interested in Social Studies, here are some majors you might like:");
+        System.out.println("- Political Science");
+        System.out.println("- History");
+        System.out.println("- Sociology");
+        System.out.println("- Anthropology");
+        System.out.println("- International Relations");
+        System.out.println("- Psychology");
+        System.out.println("- Criminal Justice");
+    }
+
+    public static void askFollowUpQuestions() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("\nDo you enjoy working with people or data more? (Type 'people' or 'data'): ");
+        String preference = input.nextLine().toLowerCase();
+
+        if (preference.equals("people")) {
+            suggestPeopleOrientedMajors();
+        } else if (preference.equals("data")) {
+            suggestDataOrientedMajors();
+        } else {
+            System.out.println("\nIt seems you're unsure. Don't worry, you can always explore many fields before making your final decision!");
+        }
+    }
+
+    public static void suggestPeopleOrientedMajors() {
+        System.out.println("\nSince you enjoy working with people, here are some major suggestions:");
+        System.out.println("- Psychology");
+        System.out.println("- Sociology");
+        System.out.println("- Education");
+        System.out.println("- Nursing");
+        System.out.println("- Human Resources");
+        System.out.println("- Social Work");
+        System.out.println("- Business Administration (HR or Marketing)");
+    }
+
+    public static void suggestDataOrientedMajors() {
+        System.out.println("\nSince you enjoy working with data, consider these major options:");
+        System.out.println("- Computer Science");
+        System.out.println("- Data Science");
+        System.out.println("- Statistics");
+        System.out.println("- Information Systems");
+        System.out.println("- Economics");
+        System.out.println("- Engineering (e.g., Industrial Engineering)");
+        System.out.println("- Business Analytics");
     }
 }
